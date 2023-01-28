7d72f4e86beb0ccb024832127f1fa97c48695e0c
S Sunday M <sarahsmoses4@gmail.com>
2023-01-29 00:18:44 +0200

    new widgets
    i.e location weather details, hourly vs weekly updates, titles

--- weather_app/lib/components/home.dart
+++ weather_app/lib/components/home.dart
@@ -1,5 +1,15 @@
+
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'widgets/hourly.text.widget.dart';
+import 'widgets/location_current.widget.dart';
+import 'widgets/updates.view.dart';
+// curved bottom navigation package to create a curved bottom nav bar with animation
+
+/// 1. define Home class (Extends StatefulWidget)
+/// 2. create new State() --> _homeState()
+/// 3. Define class _homeState (
+/// items: card--weather forecast details, bottom nav)
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,16 +24,37 @@ class _homeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 21, 2, 24),
+      // contains bottom navigation. 
+      backgroundColor: const Color.fromARGB(255, 21, 2, 24),
+      
+      
+      body:Column(
+        
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          weatherLocation("Nairobi", Icons.wb_cloudy, "22", "Saturday January 28, 2023"),
+          const SizedBox(height:60),
+          HourText("Hour Updates", "Weekly Updates"),
+          const SizedBox(height: 40,),
+          upDates("11:20am", Icons.sunny_snowing, "28.1"),
+
+
+        ],
+
+      ),
+        
+      
+
       bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Color.fromARGB(255, 21, 2, 24),
+        backgroundColor: const Color.fromARGB(255, 21, 2, 24),
+        // ignore: prefer_const_literals_to_create_immutables
         items: [
-        Icon(Icons.location_pin), 
-        Icon(Icons.home), 
-        Icon(Icons.menu), 
+        const Icon(Icons.location_pin), 
+        const Icon(Icons.home), 
+        const Icon(Icons.menu), 
         
       ],
-      animationDuration: Duration(milliseconds: 150),),
+      animationDuration: const Duration(milliseconds: 150),),
     );
   }
 }
