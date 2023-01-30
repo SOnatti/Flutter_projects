87f45d4fd37663285d4e89257c3878d612e9f446
S Sunday M <sarahsmoses4@gmail.com>
2023-01-30 02:02:06 +0200

    Update main.dart

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors, unused_field
-
 import 'package:flutter/material.dart';
 import "components/home.dart";
 
 void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
-
+    runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
